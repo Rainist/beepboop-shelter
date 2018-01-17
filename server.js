580a4524ee0898284f11538f264d30f0d7b04714
Heechul Ryu <ryuhcii@gmail.com>
2018-01-17 17:36:33 +0900

    Handle SIGTERM

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const proxy = proxyTo(slappAppHost)
 const redisRepo = require('./repo/redis')
 const express = require('express')
 const app = express()
+const terminus = require('@godaddy/terminus')
+const http = require('http')
 
 app.set('trust proxy', true)
 
@@ -17,6 +19,21 @@ app.get('/', (req, res) => {
 app.use('/api/v1', persist(redisRepo))
 app.use('/', proxy) // This must be at the end
 
-app.listen(3000, function () {
+
+const server = http.createServer(app)
+
+// this code below doesn't catch the signal when run by nodemon
+terminus(server, {
+  healthChecks: {
+    '/healthz': () => Promise.resolve()
+  },
+  timeout: 5000,
+  onShutdown: () => {
+    console.warn('being shutdown by process')
+    return Promise.resolve()
+  }
+})
+
+server.listen(3000, function () {
   console.log('listening at 3000')
 })
